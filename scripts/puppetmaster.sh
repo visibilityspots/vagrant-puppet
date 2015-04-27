e2a323b982cb406257a9f9789765193a19cac85b
Jan Collijs <jan.collijs@inuits.eu>
2015-04-27 10:11:35 +0200

    Bugfix: adding ssldir param for initial puppet apply

--- scripts/puppetmaster.sh
+++ scripts/puppetmaster.sh
@@ -3,9 +3,8 @@
 [ -f /etc/puppet/hiera.yaml ] || cp /vagrant/hieradata/hiera.yaml /etc/puppet/hiera.yaml
 
 ## Set up the puppetmaster using a puppet apply for bootstrapping or puppet agent --test when already bootstrapped
-
 if [ ! -d /etc/puppet/ssl/public_keys ]; then
-	puppet apply /etc/puppet/environments/production/manifests/ --modulepath /etc/puppet/environments/production/modules
+	puppet apply /etc/puppet/environments/production/manifests/ --modulepath /etc/puppet/environments/production/modules --ssldir /var/lib/puppet/ssl/
 else
 	puppet agent --test
 fi
