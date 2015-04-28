4e9b4ec02ccd4e82d1e7bd815f6cfced469d7f6c
Jan Collijs <jan.collijs@inuits.eu>
2015-04-28 14:47:45 +0200

    Bugfix: changed provisioning if statement to use new SSL DIR location

--- scripts/puppetmaster.sh
+++ scripts/puppetmaster.sh
@@ -3,7 +3,7 @@
 [ -f /etc/puppet/hiera.yaml ] || cp /vagrant/hieradata/hiera.yaml /etc/puppet/hiera.yaml
 
 ## Set up the puppetmaster using a puppet apply for bootstrapping or puppet agent --test when already bootstrapped
-if [ ! -d /etc/puppet/ssl/public_keys ]; then
+if [ ! -d /var/lib/puppet/ssl/public_keys ]; then
 	puppet apply /etc/puppet/environments/production/manifests/ --modulepath /etc/puppet/environments/production/modules --ssldir /var/lib/puppet/ssl/
 else
 	puppet agent --test
