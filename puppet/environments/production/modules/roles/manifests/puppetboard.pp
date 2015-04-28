ff2f28733b745aa08d426da3887d03ec0db3b355
Jan Collijs <jan.collijs@inuits.eu>
2015-04-28 16:23:28 +0200

    Bugfix: enabling epel repository for the virtualbox provider

--- puppet/environments/production/modules/roles/manifests/puppetboard.pp
+++ puppet/environments/production/modules/roles/manifests/puppetboard.pp
@@ -4,6 +4,14 @@
 
 class roles::puppetboard {
 
+  if $::virtual=='virtualbox' {
+    augeas { 'enable_epel_repo':
+      changes => [
+        'set /files/etc/yum.repos.d/epel.repo/epel/enabled 1',
+      ],
+      before  => Class['::puppetboard']
+    }
+  }
   include ::profiles::apache
   include ::profiles::puppetboard
 
