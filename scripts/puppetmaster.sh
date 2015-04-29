575178fbf89c94b2ca3dc33d14ccc02e5c519a8e
Jan Collijs <jan.collijs@inuits.eu>
2015-04-29 15:58:36 +0200

    Bugfix: running puppet after puppet apply on puppetmaster

--- scripts/puppetmaster.sh
+++ scripts/puppetmaster.sh
@@ -5,6 +5,7 @@
 ## Set up the puppetmaster using a puppet apply for bootstrapping or puppet agent --test when already bootstrapped
 if [ ! -d /var/lib/puppet/ssl/public_keys ]; then
 	puppet apply /etc/puppet/environments/production/manifests/ --modulepath /etc/puppet/environments/production/modules --ssldir /var/lib/puppet/ssl/
+	puppet agent --test
 else
 	puppet agent --test
 fi
