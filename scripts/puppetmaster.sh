42fba6357e47e15ae472578792e74939dc536d2b
Jan Collijs <jan.collijs@inuits.eu>
2015-05-04 10:34:29 +0200

    Bugfix: disabled naive autosigning and enabled basic autosigning which fixes #8

--- scripts/puppetmaster.sh
+++ scripts/puppetmaster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 [ -f /etc/puppet/hiera.yaml ] || cp /vagrant/hieradata/hiera.yaml /etc/puppet/hiera.yaml
+[ -f /etc/puppet/autosign.conf ] || cp /vagrant/puppet/autosign.conf /etc/puppet/autosign.conf
 
 ## Set up the puppetmaster using a puppet apply for bootstrapping or puppet agent --test when already bootstrapped
 if [ ! -d /var/lib/puppet/ssl/public_keys ]; then
